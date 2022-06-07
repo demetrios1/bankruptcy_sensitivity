7e637c6e42badefa60830aab86e05fd2bcf939bb
Demetrios Papakostas <34044954+demetrios1@users.noreply.github.com>
2022-06-07 09:27:43 -0400

    Add files via upload

---
+++ extra_files/contour_demo.py
@@ -0,0 +1,66 @@
+
+import scipy
+import numpy as np
+import matplotlib.pyplot as pl
+import scipy.stats as st
+#number of observations could matter here
+#rho is 0.420 from Franks code
+data = np.random.multivariate_normal((1., 1), [[1, .420], [.420, 1]], 1000000)
+
+x = data[:, 0]
+y = data[:, 1]
+xmin, xmax = -4, 3
+ymin, ymax = -3, 3.2
+
+# Peform the kernel density estimate
+xx, yy = np.mgrid[xmin:xmax:100j, ymin:ymax:100j]
+positions = np.vstack([xx.ravel(), yy.ravel()])
+values = np.vstack([x, y])
+kernel = st.gaussian_kde(values)
+f = np.reshape(kernel(positions).T, xx.shape)
+
+fig = pl.figure()
+ax = fig.gca()
+ax.set_xlim(xmin, xmax)
+ax.set_ylim(ymin, ymax)
+# Contourf plot
+cfset = ax.contourf(xx, yy, f,cmap='Blues')#colors='white')
+## Or kernel density estimate plot instead of the contourf plot
+ax.imshow(np.rot90(f), cmap='Blues', extent=[xmin, xmax, ymin, ymax])
+
+# Contour plot
+cset = ax.contour(xx, yy, f, colors='k', linewidths=0.5)
+# Label plot
+#how did we get gamma estimate?
+#it is the coefficient on gamma!
+gamma=0.187
+ax.clabel(cset, inline=1, fontsize=10)
+ax.set_xlabel('Bankruptcy Score')
+ax.set_ylabel('Going Concern Score')
+ax.hlines(0, 0, 6, lw=1, color='black')
+ax.hlines(0, 0, -6, lw=1, color='black')
+ax.vlines(0, 0, 6, lw=1, color='black')
+ax.vlines(0, 0, -6, lw=1, color='black')
+ax.vlines(-1*gamma, 0, 4, lw=1, color='black')
+ax.text(-2, -2, '$\pi_\mathrm{00}$', fontsize=12)
+ax.text(2, -2, '$\pi_\mathrm{10}$', fontsize=12)
+ax.text(-2, 2, '$\pi_\mathrm{01}$', fontsize=12)
+ax.text(2.132, 2.018, '$\pi_\mathrm{11}$',  fontsize=12)
+
+ax.text(-.4, 2.5, 'A', fontsize=12)#
+
+ax.text(-1*gamma-0.24, -0.335, '$-\gamma$', fontsize=12)
+import matplotlib.transforms as mtransforms
+trans = mtransforms.blended_transform_factory(ax.transData, ax.transAxes)
+y1positive=y>0
+d = scipy.zeros(len(y))
+
+ax.fill_betweenx(y, -1*gamma, 0, where= y>=d, 
+                facecolor='grey', alpha=0.5)
+#
+pl.ylabel('Exposure score: $\Phi^{-1}(Pr(G\mid \mathbf{x}))$')
+pl.xlabel('Outcome score: $\Phi^{-1}(Pr(B\mid \mathbf{x}))$')
+#pl.savefig('C:/Users/demetri/Documents/research/comp/bivar_fit_2_comp_bank.jpg')
+#pl.savefig('C:/Users/demetri/Documents/research/comp/bivar_fit_2.pdf')
+pl.savefig("C:/Users/demetri/Dropbox/dissertation/figures/bivar_norm_2.png")
+pl.show()
---
+++ extra_files/contour_demo_jointplot.py
@@ -0,0 +1,49 @@
+import numpy as np 
+import matplotlib.pyplot as plt
+import seaborn as sns; sns.set()
+import matplotlib as mpl
+import pandas as pd
+mpl.style.use('seaborn-white')
+# Define dimension. 
+d = 2
+# Set mean vector. 
+m = np.array([1, 2]).reshape(2, 1)
+
+# Set covariance function. 
+K_0 = np.array([[2, 1],
+                [1, 2]])
+
+np.linalg.eigvals(K_0)
+
+# Define epsilon.
+epsilon = 0.0001
+
+# Add small pertturbation. 
+K = K_0 + epsilon*np.identity(d)
+
+L = np.linalg.cholesky(K)
+L
+
+np.dot(L, np.transpose(L))
+
+# Number of samples. 
+n = 10000
+
+u = np.random.normal(loc=0, scale=1, size=d*n).reshape(d, n)
+
+x = m + np.dot(L, u)
+
+####
+
+B=x[0]
+G=x[1]
+
+
+sns.jointplot(x=B,
+              y=G, 
+             kind="kde", space=0);
+#plt.xlabel('$P(B\mid \mathbf{x})$')
+#plt.ylabel('$P(G\mid \mathbf{x})$')
+
+#plt.savefig('C:/Users/demetri/Documents/ASU/dissertation/going_vs_bank.pdf')
+plt.show()
